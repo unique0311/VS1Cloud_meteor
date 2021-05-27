47ba88ecca91f2d2d023abe89e8ebba84aa1fb4c
VS1_Bruno <bruno@trueerp.com>
2021-05-27 16:10:29 +0200

    Negative values allowed in new stock adjustment

--- client/inventory/stockadjustment/frm_stockAdjustment.js
+++ client/inventory/stockadjustment/frm_stockAdjustment.js
@@ -1234,7 +1234,7 @@ Template.stockadjustmentcard.events({
             (event.keyCode >= 96 && event.keyCode <= 105) ||
             event.keyCode == 8 || event.keyCode == 9 ||
             event.keyCode == 37 || event.keyCode == 39 ||
-            event.keyCode == 46 || event.keyCode == 190) {
+            event.keyCode == 46 || event.keyCode == 190 || event.keyCode == 189 || event.keyCode == 109) {
         } else {
             event.preventDefault();
         }
