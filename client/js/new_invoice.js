6044294c55f80de0c2f2784cd018f3befe38241a
trueerp_cloud <devsa@trueerp.com>
2021-07-29 16:47:06 +0200

    Rasheed Commit Tax Rate Modal

--- client/js/new_invoice.js
+++ client/js/new_invoice.js
@@ -4237,7 +4237,10 @@ Template.new_invoice.onRendered(function () {
                                 [10, 25, 50, "All"]
                             ],
                             info: true,
-                            responsive: true
+                            responsive: true,
+                            "fnInitComplete": function () {
+                                $("<button class='btn btn-primary btnRefreshProduct' type='button' id='btnRefreshProduct' style='padding: 4px 10px; font-size: 14px; margin-left: 8px !important;'><i class='fas fa-search-plus' style='margin-right: 5px'></i>Search</button>").insertAfter("#tblInventory_filter");
+                            }
 
                         });
 
