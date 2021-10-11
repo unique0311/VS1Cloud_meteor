6ed2a41447f9d47c6b63164ac7a28954cd8652d9
trueerp_cloud <devsa@trueerp.com>
2021-10-11 12:54:50 +0200

    Rasheed Commit

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const HTMLToCache = '/';
-const version = 'MSW V0.47';
+const version = 'MSW V0.48';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(version).then((cache) => {
