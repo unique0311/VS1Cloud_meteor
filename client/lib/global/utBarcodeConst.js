a1bfbd62354996edfdc3e48c4427ec4decb6d104
trueerp_cloud <devsa@trueerp.com>
2021-12-09 08:59:48 +0200

    Rasheed Commit

--- client/lib/global/utBarcodeConst.js
+++ client/lib/global/utBarcodeConst.js
@@ -33,7 +33,7 @@
  licenceIPAddress = "login.vs1cloud.com"; //165.228.147.127
   //Global Declaration
   /* VS1 SandBox Details */
-  URLRequest = 'https://'; //non ssl server
+  URLRequest = 'http://'; //non ssl server
   checkSSLPorts = '4433'; //Non SSL Port
   vs1loggedDatatbase = 'vs1_sandbox_license'; //SandBox databaseName
 
