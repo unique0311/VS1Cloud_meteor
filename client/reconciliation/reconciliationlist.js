15902c46a968bf21d78e775058e7b14c5e4dabab
trueerp_cloud <devsa@trueerp.com>
2022-03-15 14:40:07 +0200

    Rasheed Commit

--- client/reconciliation/reconciliationlist.js
+++ client/reconciliation/reconciliationlist.js
@@ -419,7 +419,6 @@ Template.reconciliationlist.onRendered(function() {
                 let useData = data.treconciliationlist;
                 let lineItems = [];
                 let lineItemObj = {};
-                console.log(data);
                 if(data.Params.IgnoreDates == true){
                   $('#dateFrom').attr('readonly', true);
                   $('#dateTo').attr('readonly', true);
