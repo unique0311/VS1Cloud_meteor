efd29e1bc4693ccab6817960d0b00d5e802afa5a
trueerp_cloud <devsa@trueerp.com>
2022-03-15 15:00:55 +0200

    Rasheed Commit

--- client/lib/global/indexdbstorage.js
+++ client/lib/global/indexdbstorage.js
@@ -448,7 +448,7 @@ getStoreToDelete = async function (email) {
 openDbCheckVersion = async function () {
   var promiseversion =  new Promise((resolve, reject) => {
     var versionExists = false;
-    let dbReqVersion = indexedDB.open('TDatabaseVersion', 5);
+    let dbReqVersion = indexedDB.open('TDatabaseVersion', 6);
     dbReqVersion.onsuccess = function () {
      resolve(versionExists);
     };
