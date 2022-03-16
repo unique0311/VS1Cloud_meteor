1741ef6acfa911688c9d0c0d338cc7a20533d792
Fatih Gucuko <fatih.gucuko@webinify.com>
2022-03-16 23:00:05 +0100

    work in progress, request object built, awaiting API endpoint details for saving

--- client/reports/profitandloss/layoutEditor.js
+++ client/reports/profitandloss/layoutEditor.js
@@ -8,25 +8,103 @@ export default class layoutEditor {
 
     this.save = element.querySelector(".saveTable"); // save button
 
+    this.accountList = [];
+
     this.bindEvents();
     console.log("Layout editor loaded");
   }
 
-  setupTree() {
+  /**
+   * This will setup the object in the html dom
+   */
+  async setupTree() {
     const sortableAccounts = document.querySelectorAll(".sortableAccount");
     sortableAccounts.forEach((sortableAccount) => {
-        const accountType = sortableAccount.querySelector('.avoid').getAttribute('account-type');
-        sortableAccount.setAttribute('account-type', accountType);
+      const accountType = sortableAccount
+        .querySelector(".avoid")
+        .getAttribute("account-type");
+      sortableAccount.setAttribute("account-type", accountType);
 
-        const draggables = sortableAccount.querySelectorAll('.draggable');
-        draggables.forEach((draggable) => {
-            draggable.setAttribute('account-type', accountType);
-        })
-    })
+      const draggables = sortableAccount.querySelectorAll(".draggable");
+      draggables.forEach((draggable) => {
+        draggable.setAttribute("account-type", accountType);
+      });
+    });
   }
 
-  buildTree() {
+  /**
+   * Build the object of the tree
+   */
+  async buildTree() {
     const sortableAccounts = document.querySelectorAll(".sortableAccount");
+    console.log(sortableAccounts);
+    let accounts = [];
+
+    const jsonExpected = [
+      {
+        "account-type": "type",
+        position: 8,
+        "sub-accounts": [
+          {
+            "account-id": 7,
+            "account-type": "my-type",
+            position: 1,
+          },
+          {
+            "account-id": 7,
+            "account-type": "my-type",
+            position: 1,
+          },
+        ],
+      },
+    ];
+
+    sortableAccounts.forEach((el) => {
+      const position = el.getAttribute("position");
+      const accountType = el.getAttribute("account-type");
+
+      let sortableAccount = {
+        position: position,
+        accountType: accountType,
+        "sub-accounts": [],
+      };
+      const subAccounts = el.querySelectorAll(".draggable");
+
+      subAccounts.forEach((ele) => {
+        sortableAccount["sub-accounts"].push({
+          "account-id": ele.getAttribute("account-id"),
+          "account-type": ele.getAttribute("account-type"),
+          position: ele.getAttribute("position"),
+        });
+      });
+
+      accounts.push(sortableAccount);
+    });
+
+    this.accountList = accounts;
+
+    console.log(this.accountList);
+  }
+
+  /**
+   * Save the jsonObject to database via the API
+   */
+  async saveData() {
+    let formData = new FormData();
+    formData.set("accounts", this.accountList);
+
+    const response = await fetch("my-url", {
+      method: 'POST',
+      body: formData,
+    });
+
+    if (response.ok) {
+      let data = await response.json();
+      // then refresh page
+      window.location.reload();
+    } else {
+      console.log(response);
+    }
   }
 
   bindEvents() {
@@ -34,10 +112,11 @@ export default class layoutEditor {
     $(this.save).on("click", (e) => {
       e.preventDefault();
       console.log(e);
-      const sortableAccounts =
-        document.querySelectorAll(".sortableAccount");
+      const sortableAccounts = document.querySelectorAll(".sortableAccount");
       console.log(sortableAccounts.length);
-      this.setupTree();
+      this.setupTree().then(() => this.buildTree()).then(() => this.saveData());
+      // setTimeout(() => this.buildTree(), 150);
+      // setTimeout(() => this.saveData(), 300);
 
       // this will add a position attribute to each sortableAccount
       for (let i = 0; i < sortableAccounts.length; i++) {
