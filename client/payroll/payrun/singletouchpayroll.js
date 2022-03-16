c438a678a4d32fc128d5890d1c21703b13c2eada
bruno@vs1cloud.com <bruno@vs1cloud.com>
2022-03-16 12:49:20 +0200

    Bruno commit

--- client/payroll/payrun/singletouchpayroll.js
+++ client/payroll/payrun/singletouchpayroll.js
@@ -19,11 +19,11 @@ import {
 import 'jquery-editable-select';
 let utilityService = new UtilityService();
 let sideBarService = new SideBarService();
-Template.payrun.onCreated(function() {
+Template.singletouchpayroll.onCreated(function() {
 
 });
 
-Template.payrun.onRendered(function() {
+Template.singletouchpayroll.onRendered(function() {
     $("#date-input,#dateTo,#dateFrom").datepicker({
         showOn: 'button',
         buttonText: 'Show Date',
@@ -38,7 +38,7 @@ Template.payrun.onRendered(function() {
     });
 
     setTimeout(function() {
-        $('#tblPayRunHistory').DataTable({
+        $('#tblSingleTouchPayroll').DataTable({
             columnDefs: [{
                 "orderable": false,
                 "targets": -1
@@ -84,79 +84,7 @@ Template.payrun.onRendered(function() {
                 [0, "asc"]
             ],
             action: function() {
-                $('#tblPayRunHistory').DataTable().ajax.reload();
-            },
-            "fnDrawCallback": function(oSettings) {
-                setTimeout(function() {
-                    MakeNegative();
-                }, 100);
-            },
-
-        }).on('page', function() {
-            setTimeout(function() {
-                MakeNegative();
-            }, 100);
-            let draftRecord = templateObject.datatablerecords.get();
-            templateObject.datatablerecords.set(draftRecord);
-        }).on('column-reorder', function() {
-
-        }).on('length.dt', function(e, settings, len) {
-            setTimeout(function() {
-                MakeNegative();
-            }, 100);
-        });
-
-        $('.fullScreenSpin').css('display', 'none');
-    }, 0);
-
-    setTimeout(function() {
-        $('#tblPayRunList').DataTable({
-            columnDefs: [{
-                "orderable": false,
-                "targets": -1
-            }],
-            "sDom": "<'row'><'row'<'col-sm-12 col-md-6'f><'col-sm-12 col-md-6'l>r>t<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>B",
-            buttons: [{
-                extend: 'excelHtml5',
-                text: '',
-                download: 'open',
-                className: "btntabletocsv hiddenColumn",
-                filename: "taxratelist_" + moment().format(),
-                orientation: 'portrait',
-                exportOptions: {
-                    columns: ':visible'
-                }
-            }, {
-                extend: 'print',
-                download: 'open',
-                className: "btntabletopdf hiddenColumn",
-                text: '',
-                title: 'Tax Rate List',
-                filename: "taxratelist_" + moment().format(),
-                exportOptions: {
-                    columns: ':visible'
-                }
-            }],
-            select: true,
-            destroy: true,
-            colReorder: true,
-            colReorder: {
-                fixedColumnsRight: 1
-            },
-            lengthMenu: [
-                [25, -1],
-                [25, "All"]
-            ],
-            // bStateSave: true,
-            // rowId: 0,
-            paging: true,
-            info: true,
-            responsive: true,
-            "order": [
-                [0, "asc"]
-            ],
-            action: function() {
-                $('#tblPayRunList').DataTable().ajax.reload();
+                $('#tblSingleTouchPayroll').DataTable().ajax.reload();
             },
             "fnDrawCallback": function(oSettings) {
                 setTimeout(function() {
@@ -182,18 +110,13 @@ Template.payrun.onRendered(function() {
     }, 0);
 });
 
-Template.payrun.events({
-    'click .btnPayRunNext': function (event) {
+Template.singletouchpayroll.events({
+    'click .colPayRunDetailsFirstName': function (event) {
         $('.modal-backdrop').css('display', 'none');
-        FlowRouter.go('/payrundetails');
+        FlowRouter.go('/payslip');
     },
-    'click .btnSingleTouchPayroll': function (event) {
-        $('.modal-backdrop').css('display', 'none');
-        FlowRouter.go('/singletouchpayroll');
-    },
-
 });
 
-Template.payrun.helpers({
+Template.singletouchpayroll.helpers({
 
 });
