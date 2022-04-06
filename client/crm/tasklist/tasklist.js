8625d2ec4153d92b0de0d8026afa7a3810f0b74b
bruno@vs1cloud.com <bruno@vs1cloud.com>
2022-04-06 16:19:45 +0200

    Bruno commit

--- client/crm/tasklist/tasklist.js
+++ client/crm/tasklist/tasklist.js
@@ -30,8 +30,16 @@ Template.tasklist.onRendered(function() {
 });
 
 Template.tasklist.events({
-    'click .taskCol': function(event) {
+    'click .mainTaskCol': function(event) {
         $('#taskModal').modal('toggle');
+    },
+    'click .btnAddSubTask': function(event) {
+        $(".newTaskRow"). css("display", "inline-flex");
+        $(".btnAddSubTask"). css("display", "none");
+    },
+    'click .btnCancelAddTask': function(event) {
+        $(".btnAddSubTask"). css("display", "block");
+        $(".newTaskRow"). css("display", "none");
     }
 });
 
