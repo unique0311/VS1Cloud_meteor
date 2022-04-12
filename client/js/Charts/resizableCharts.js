ea81f2f2c0f25a70c6294f23d303caec309a19b2
Fatih Gucuko <fatih.gucuko@webinify.com>
2022-04-12 21:31:35 +0200

    each charts will now keeps the aspect ratio on height/width

--- client/js/Charts/resizableCharts.js
+++ client/js/Charts/resizableCharts.js
@@ -7,7 +7,14 @@ export default class resizableCharts {
         minWidth:250,
         // aspectRatio: 1.5 / 1
         handles: "e",
+        resize: function( event, ui ) {
+          $(ui.element[0]).css('height', "");
+         // console.log(event.currentTarget);
+          //console.log(ui.element[0]);
+        }
       });
+      
+      
     }, timeOut);
   }
 
