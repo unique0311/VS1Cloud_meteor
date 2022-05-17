c60d647a24cd133204990617ad20dc55baf5a7bd
trueerp_cloud <devsa@trueerp.com>
2022-05-17 15:35:35 +0200

    Vs1 Cloud Build: Version: 2022.05.05 - 05/17 - VS1Cloud

--- client/lib/global/indexdbstorage.js
+++ client/lib/global/indexdbstorage.js
@@ -461,7 +461,7 @@ getStoreToDelete = async function (email) {
 openDbCheckVersion = async function () {
   var promiseversion =  new Promise((resolve, reject) => {
     var versionExists = false;
-    let dbReqVersion = indexedDB.open('TDatabaseVersion', 25);
+    let dbReqVersion = indexedDB.open('TDatabaseVersion', 26);
     dbReqVersion.onsuccess = function () {
      resolve(versionExists);
     };
