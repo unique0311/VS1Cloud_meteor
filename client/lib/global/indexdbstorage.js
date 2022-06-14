b8da7bdf30e6fea25dddcead359ffdc0128c71bd
trueerp_cloud <devsa@trueerp.com>
2022-06-14 17:25:08 +0200

    no message

--- client/lib/global/indexdbstorage.js
+++ client/lib/global/indexdbstorage.js
@@ -484,7 +484,7 @@ getStoreToDelete = async function (email) {
 openDbCheckVersion = async function () {
   var promiseversion =  new Promise((resolve, reject) => {
     var versionExists = false;
-    let dbReqVersion = indexedDB.open('TDatabaseVersion', 37);
+    let dbReqVersion = indexedDB.open('TDatabaseVersion', 38);
     dbReqVersion.onsuccess = function () {
      resolve(versionExists);
     };
