f11e0f2e0591391f950729985e777276c0382184
Fatih Gucuko <fatih@zuusoft.com>
2022-06-15 18:19:16 +0200

    adding loading overlay function

---
+++ client/LoadingOverlay.js
@@ -0,0 +1,25 @@
+class LoadingOverlay {
+  /**
+   * 
+   * @param {number} timeout 
+   */
+  show(timeout = 0) {
+    setTimeout(() => {
+      $(".fullScreenSpin").css("display", "");
+      console.log("Showing loading overlay");
+    }, timeout);
+  }
+
+  /**
+   * 
+   * @param {number} timeout 
+   */
+  Hide(timeout = 1000) {
+    setTimeout(() => {
+      $(".fullScreenSpin").css("display", "none");
+      console.log("Hiding loading overlay");
+    }, timeout);
+  }
+}
+
+export default loadingOverlay = new LoadingOverlay();
