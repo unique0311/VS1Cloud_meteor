47c7740bc43ba0393e32b4fe1702f46041b59f8c
Fatih Gucuko <fatih@zuusoft.com>
2022-06-29 11:14:32 +0200

    hidding default currency

---
+++ client/packages/currency/modals/FxRateModal.js
@@ -0,0 +1,23 @@
+import "../../../lib/global/indexdbstorage.js";
+
+let defaultCurrencyCode = CountryAbbr; // global variable "AUD"
+
+Template.FxRateModal.onCreated(function () {
+  const templateObject = Template.instance();
+});
+
+Template.FxRateModal.onRendered(function () {
+  console.log("Fx Rate Modal");
+  let templateObject = Template.instance();
+});
+
+Template.FxRateModal.events({});
+
+Template.FxRateModal.helpers({
+  isDefaultCurrency: (currencyData) => {
+    //console.log(currencyData);
+    return currencyData.currency == defaultCurrencyCode
+      ? "hidden"
+      : "";
+  },
+});
